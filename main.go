9f262ad912b244cbc4fd18ef5e97f621034c4954
xasancodeb <124950064+xasancodeb@users.noreply.github.com>
2024-01-25 21:45:18 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,35 +1,30 @@
 package main
-
 import (
-	"fmt"
-	"time"
+    “fmt”
+    “time”
 )
-
 func main() {
-
-	var input int
-
-	locations := []string{"Gotham city", "Metroplis", "Queens", "Little Land", "Vice City"}
-	superPowers := []string{"Laser eyes", "Flying", "Invisibility", "Power Absobtion", "Fire", "Freezing", "SuperHuman Speed"}
-
-	fmt.Println("Welcome to the world superheroes with superpowers")
-	fmt.Println("Your choice will unveil your superpower:")
-
-	//Using current time as a seed
-	seed := time.Now().UnixNano()
-
-	//User input
-
-	fmt.Println("Choose a number.")
-	fmt.Scan(&input)
-
-	//Modifying the seed with user input
-	seed = (seed + int64(input)) % 100
-
-	//Selecting the strings based on modified seed
-
-	choice1 := locations[seed%int64(len(locations))]
-	choice2 := superPowers[seed%int64(len(superPowers))]
-
-	fmt.Printf("Your superpower is %s in %s.\n", choice2, choice1)
+    var input int
+	
+    superheroes := []string{“Batman”, “Superman”, “Spiderman”, “Ironman”, “Wonder Women”}
+    villans := []string{“The Joker”, “Loki”, “Thanos”, “Darth Vader”, “Voldemort”}
+    Quests := []string{“Catch “, “....“, “kill”, “save”, “live”, “go”, “rage”}
+    fmt.Println(“Hey...Hey you do you want to become a superhero? “)
+    fmt.Println(“I will give you a quest and a vilin to defeat :“)
+	
+    //Using current time as a seed
+    seed := time.Now().UnixNano()
+    //User input
+    fmt.Println(“Choose a number.“)
+    fmt.Scan(&input)
+	
+    //Modifying the seed with user input
+    seed = (seed + int64(input)) % 100
+	
+    //Selecting the strings based on modified seed
+    choice1 := superheroes[seed%int64(len(superheroes))]
+    choice2 := villans[seed%int64(len(villans))]
+    Choise3 := Quests[seed%int64(len(Quests))]
+	
+    fmt.Printf(“Your superpower is %s in %s and %s \n”, choice2, choice1, Choise3)
 }
